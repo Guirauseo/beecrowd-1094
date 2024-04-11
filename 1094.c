9ee0aa327559db7cb4825cfb86bc84e7b356e164
Guilherme Rauseo <166748221+Guirauseo@users.noreply.github.com>
2024-04-11 16:17:20 -0300

    Update 1094.c

--- 1094.c
+++ 1094.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
-
-int main() {
-    int casos, quantidade;
-    char tipo;
-    int total_cobaias = 0, total_coelhos = 0, total_ratos = 0, total_sapos = 0;
-
-    scanf("%d", &casos);
-
-    for (int i = 0; i < casos; i++) {
-        scanf("%d %c", &quantidade, &tipo);
-        total_cobaias += quantidade;
-
-        if (tipo == 'C') {
-            total_coelhos += quantidade;
-        } else if (tipo == 'R') {
-            total_ratos += quantidade;
-        } else if (tipo == 'S') {
-            total_sapos += quantidade;
-        }
-    }
-
-    printf("Total: %d cobaias\n", total_cobaias);
-    printf("Total de coelhos: %d\n", total_coelhos);
-    printf("Total de ratos: %d\n", total_ratos);
-    printf("Total de sapos: %d\n", total_sapos);
-    printf("Percentual de coelhos: %.2f %%\n", (float)total_coelhos / total_cobaias * 100);
-    printf("Percentual de ratos: %.2f %%\n", (float)total_ratos / total_cobaias * 100);
-    printf("Percentual de sapos: %.2f %%\n", (float)total_sapos / total_cobaias * 100);
-
-    return 0;
+ 
+int main(){
+ int casos,C=0,R=0,S=0,cobaias=0,quantidade;
+ char tipo;
+ scanf("%d",&casos);
+ while(casos>0)
+{
+scanf("%d %c",&quantidade,&tipo);
+cobaias+=quantidade;
+if(tipo=='C')
+{
+C+=quantidade;
+}
+else
+if(tipo=='S')
+{
+S+=quantidade;
+}
+else
+if(tipo=='R')
+{
+R+=quantidade;
+}
+casos--;
+}
+printf("Total: %d cobaias\n",cobaias);
+printf("Total de coelhos: %d\n",C);
+printf("Total de ratos: %d\n",R);
+printf("Total de sapos: %d\n",S);
+printf("Percentual de coelhos: %.2f %%\n",(float)C/cobaias*100);
+printf("Percentual de ratos: %.2f %%\n",(float)R/cobaias*100);
+printf("Percentual de sapos: %.2f %%\n",(float)S/cobaias*100);
+return 0;
 }
